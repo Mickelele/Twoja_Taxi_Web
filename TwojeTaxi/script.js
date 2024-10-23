5daf03134246bddf1aac0e3a7a9b6388cc0ba7cc
Mickelele <162701318+Mickelele@users.noreply.github.com>
2024-10-23 22:32:30 +0200

    Add files via upload

---
+++ TwojeTaxi/script.js
@@ -0,0 +1,9 @@
+document.getElementById("menu-icon").addEventListener("click", function() {
+    var menu = document.getElementById("dropdown-menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block"; 
+    }
+});
+
